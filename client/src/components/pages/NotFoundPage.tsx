df953e3f4f85539ef062d5439806eb533c0ddbad
Szymon Wiszczuk <golota60@gmail.com>
2020-04-29 19:39:00 +0200

    lint fix

--- client/src/components/pages/NotFoundPage.tsx
+++ client/src/components/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './NotFoundPage.scss';
 
 const NotFoundPage = () => {
-  return <>You shouldn't be here. Why are you testing so hard?</>
+  return <>You shouldn't be here. Why are you testing so hard?</>;
 };
 
 export default NotFoundPage;
